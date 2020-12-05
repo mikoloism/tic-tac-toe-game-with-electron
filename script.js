add085d1feeb2ca2baf40c036be8a5daf0fbbf1c
miko <mikoloism.github@gmail.com>
2020-12-05 18:24:27 +0330

    update script.js applied prettier

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const ready = (function () {
 	let player = 'o';
 	let won = null;
 
-	const hideModal =()=> { restart(); }
+	const hideModal = () => {
+		restart();
+	};
 	const restart = () => {
 		location.reload();
 	};
@@ -11,7 +13,7 @@ const ready = (function () {
 		let modal = document.createElement('div');
 		modal.classList.add('modal');
 		modal.id = 'modal';
-		let playerSpan =  document.createElement('span');
+		let playerSpan = document.createElement('span');
 		playerSpan.classList.add('player');
 		playerSpan.classList.add(whoWon);
 		playerSpan.innerHTML = whoWon;
@@ -19,65 +21,64 @@ const ready = (function () {
 		modal.appendChild(playerSpan);
 		document.body.appendChild(modal);
 		modal.onclick = hideModal;
-	}
-
+	};
 
 	const setDraw = () => {
 		document.body.classList.add('draw');
 		showModal('Draw');
-	}
+	};
 	const checkDraw = () => {
 		players = [];
 		Array.from(placeholders).forEach((holder) => {
-			if(holder.dataset.player === 'null')
-				players.push(holder);
+			if (holder.dataset.player === 'null') players.push(holder);
 		});
-		if( players.length === 0 && won === null ) setDraw();
+		if (players.length === 0 && won === null) setDraw();
 	};
 
 	const checkWon = (items, whichPlayer) => {
 		let [i1, i2, i3] = items;
-		if(
+		if (
 			placeholders[i1].dataset.player === whichPlayer &&
 			placeholders[i2].dataset.player === whichPlayer &&
 			placeholders[i3].dataset.player === whichPlayer &&
-			placeholders[i1].dataset.player === placeholders[i3].dataset.player &&
-			placeholders[i1].dataset.player === placeholders[i2].dataset.player &&
+			placeholders[i1].dataset.player ===
+				placeholders[i3].dataset.player &&
+			placeholders[i1].dataset.player ===
+				placeholders[i2].dataset.player &&
 			placeholders[i2].dataset.player === placeholders[i3].dataset.player
-		){
+		) {
 			placeholders[i1].classList.add('won');
 			placeholders[i2].classList.add('won');
 			placeholders[i3].classList.add('won');
 			won = whichPlayer;
 			document.body.classList.add(`${won}-won`);
 			showModal(won);
-		}
-		else checkDraw();
-	}
+		} else checkDraw();
+	};
 	const checkHolders = () => {
-		checkWon([0,4,8], 'x');
-		checkWon([2,4,6], 'x');
+		checkWon([0, 4, 8], 'x');
+		checkWon([2, 4, 6], 'x');
 
-		checkWon([0,1,2], 'x');
-		checkWon([3,4,5], 'x');
-		checkWon([6,7,8], 'x');
+		checkWon([0, 1, 2], 'x');
+		checkWon([3, 4, 5], 'x');
+		checkWon([6, 7, 8], 'x');
 
-		checkWon([0,3,6], 'x');
-		checkWon([1,4,7], 'x');
-		checkWon([2,5,8], 'x');
+		checkWon([0, 3, 6], 'x');
+		checkWon([1, 4, 7], 'x');
+		checkWon([2, 5, 8], 'x');
 
 		// ============================
 
-		checkWon([0,4,8], 'o');
-		checkWon([2,4,6], 'o');
+		checkWon([0, 4, 8], 'o');
+		checkWon([2, 4, 6], 'o');
 
-		checkWon([0,1,2], 'o');
-		checkWon([3,4,5], 'o');
-		checkWon([6,7,8], 'o');
+		checkWon([0, 1, 2], 'o');
+		checkWon([3, 4, 5], 'o');
+		checkWon([6, 7, 8], 'o');
 
-		checkWon([0,3,6], 'o');
-		checkWon([1,4,7], 'o');
-		checkWon([2,5,8], 'o');
+		checkWon([0, 3, 6], 'o');
+		checkWon([1, 4, 7], 'o');
+		checkWon([2, 5, 8], 'o');
 	};
 
 	const updateText = (clickedElement, whichPlayer) => {
@@ -89,7 +90,7 @@ const ready = (function () {
 	Array.from(placeholders).forEach((holder) => {
 		holder.onclick = function () {
 			if (this.dataset.player === 'null') {
-				player = (player === 'o' ? 'x' : 'o');
+				player = player === 'o' ? 'x' : 'o';
 				updateText(this, player);
 				checkHolders();
 			}
