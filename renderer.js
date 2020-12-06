d0c80018ea18b4cadc19d56ea3feeabe823ef0d3
miko <mikoloism.github@gmail.com>
2020-12-06 19:06:41 +0330

    Update renderer.js
    update event.keyCode from string qoute to whole number
    
    add Exit and Fullscreen key shortcut event trigger

--- renderer.js
+++ renderer.js
@@ -3,12 +3,16 @@ const { ipcRenderer } = electron;
 
 document.addEventListener('keydown', (e) => {
 	e = window.event ? window.event : e;
-	if (e.key == 'F1' || e.keyCode == '112') {
+	if (e.key == 'F1' || e.keyCode == 112) {
 		ipcRenderer.invoke('ToggleDevTools');
 	}
-	if (e.key == 'F5' || e.keyCode == '116') {
+	if (e.key == 'F5' || e.keyCode == 116) {
 		ipcRenderer.invoke('RefreshWindow');
 	}
+	if (e.key == 'F11' || e.keyCode == 122) {
+		ipcRenderer.invoke('FullScreenWindow');
+	}
+	if (e.key == 'Escape' || e.keyCode == 27) ipcRenderer.invoke('ExitApp');
 });
 document.addEventListener('mousedown', (e) => {
 	if (e.button == 2) {
