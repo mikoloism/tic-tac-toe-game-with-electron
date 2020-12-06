8ee00ea2d6d10846a65f6509c453825e49e95786
miko <mikoloism.github@gmail.com>
2020-12-06 22:14:37 +0330

    Update sound.js
    Update to Promise Object insted of Async/Await
    
    # because async/await is for ES6

--- sound.js
+++ sound.js
@@ -1,19 +1,22 @@
-document.onclick = async () => {
-	try {
+document.onclick = () => {
+	const palyAudio = new Promise((resolve, reject) => {
 		const mp3 = {},
 			wav = {};
 		wav.src = './assets/click-sound-effect.wav';
 		mp3.src = './assets/click-sound-effect.mp3';
 		wav.audio = new Audio(wav.src);
 		mp3.audio = new Audio(mp3.src);
-		await mp3.audio.play();
-		let timeOute;
-		timeOute = window.setTimeout(() => {
-			mp3.audio.pause();
-			mp3.audio.currentTime = 0;
-		}, 2000);
-		window.clearTimeout(timeOute);
-	} catch (e) {
-		console.log('Error@');
-	}
+		resolve({ mp3, wav });
+	});
+
+	const timeOut = { delay: 1000 };
+	palyAudio
+		.then(({ mp3 } = {}) => mp3.audio.play())
+		.then(({ mp3 } = {}) => {
+			timeOut.handle = window.setTimeout(() => {
+				mp3.audio.pause();
+				mp3.audio.currentTime = 0;
+			}, timeOut.delay);
+		})
+		.then(() => window.clearTimeout(timeOut.handle));
 };
