3b474c6f7ab2894612c6ecd5a18012ca6ad74799
miko <mikoloism.github@gmail.com>
2020-12-07 00:31:29 +0330

    Update sound.js
    change play sound mechanism from one file to any section
    
    Add playSound method to run anyhere (reusable method)

--- sound.js
+++ sound.js
@@ -1,20 +1,14 @@
-const holders = document.getElementsByClassName('placeholder');
-Array.from(holders).forEach((holder) =>
-	holder.addEventListener('click', function () {
-		try {
-			const mp3 = {},
-				wav = {};
-			wav.src = './assets/click-sound-effect.wav';
-			mp3.src = './assets/click-sound-effect.mp3';
-			wav.audio = new Audio(wav.src);
-			mp3.audio = new Audio(mp3.src);
-			if (this.dataset.player == 'null') mp3.audio.play();
-			window.setTimeout(() => {
-				mp3.audio.pause();
-				mp3.audio.currentTime = 0;
-			}, 1000);
-		} catch (err) {
-			console.error(err);
-		}
-	}),
-);
+const playSound = ({
+	name,
+	duration = 500,
+	format = 'mp3',
+	volume = 1,
+} = {}) => {
+	const audio = new Audio(`./assets/${name}.${format}`);
+	audio.play();
+	audio.volume = volume;
+	window.setTimeout(() => {
+		audio.pause();
+		audio.currentTime = 0;
+	}, duration);
+};
