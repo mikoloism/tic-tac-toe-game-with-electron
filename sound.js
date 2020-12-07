48c3f43180ca931c9b161b46ba9265c6b99441c2
miko <mikoloism.github@gmail.com>
2020-12-07 14:40:53 +0330

    Update sound.js
    add loop mode

--- sound.js
+++ sound.js
@@ -3,12 +3,15 @@ const playSound = ({
 	duration = 500,
 	format = 'mp3',
 	volume = 1,
+	loop = false,
 } = {}) => {
 	const audio = new Audio(`./assets/${name}.${format}`);
 	audio.play();
 	audio.volume = volume;
-	window.setTimeout(() => {
-		audio.pause();
-		audio.currentTime = 0;
-	}, duration);
+	audio.loop = loop;
+	if (!!duration)
+		window.setTimeout(() => {
+			audio.pause();
+			audio.currentTime = 0;
+		}, duration);
 };
